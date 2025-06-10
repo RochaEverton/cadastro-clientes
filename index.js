6512213dbcf0fa4dfe84a68638e6d9e1de4b6905
RochaEverton <rochaevertondev@gmail.com>
2025-06-10 13:27:20 -0300

    validateName

--- index.js
+++ index.js
@@ -9,14 +9,35 @@ async function listCustomers() {
     await rl.question('Pressione Enter para continuar...');
     printMenu();
 }
+function validateName(name) {
+    if (!name) return false;
+    if (name.trim().indexOf(' ') === -1) return false;
+    if (name.trim().length < 3) return false;
+    if (name.trim().length > 50) return false;
+    const regex = /^[a-zA-ZÀ-ÿ\s]+$/;
+    if (!regex.test(name.trim())) return false;
+    return true;
+}
+
+function validateAddress(address) {
+    if (!address) return false;
+    if (address.trim().length < 5) return false;
+    return true;
+}
 
 async function startRegistration() {
     console.clear();
-    const name = await rl.question('Digite o nome do cliente: ');
+
+    let name = '';
+    do   {
+        name = await rl.question('Qual o nome e sobrenome do cliente? ');
+        if(!validateName(name)) console.log('Nome inválido. Por favor, tente novamente.');
+    } while (!validateName(name));
+
     const address = await rl.question('Digite o endereço do cliente: ');
     const id = customers.length > 0 ? customers[customers.length - 1].id + 1 : 1;
     // Aqui você pode adicionar lógica para salvar os dados do cliente
-    console.log(`Cliente cadastrado: ${name}, Endereço: ${address}, Id: ${id}`);
+    console.log(`Cliente cadastrado: Nome: ${name}, Endereço: ${address}, Id: ${id}`);
     customers.push({name, address,id });
     await rl.question('Pressione Enter para continuar...');
     printMenu();
